4ae58b5d3a443eb658af3ef21de4236534dbaa35
Aaron Byrne <c15709609@mydit.ie>
2019-04-03 09:52:51 +0000

    Exporting admin card

--- startNetwork.sh
+++ startNetwork.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-composer-rest-server -c admin@identiphone-network -t -n never -u true -w true -a true -m true
+nohup composer-rest-server -c admin@identiphone-network -t -n never -u true -w true -a true -m true > rest-server.out 2> rest-server.err < /dev/null &
+
