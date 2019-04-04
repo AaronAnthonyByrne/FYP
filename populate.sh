af00ea5a016de031b4a6f5bee185e925da243e56
Aaron Byrne <c15709609@mydit.ie>
2019-04-04 11:15:42 +0000

    Adding commits

--- populate.sh
+++ populate.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+#Consortium Admin
+composer participant add -c admin@identiphone-network -d '{
+"$class": "org.hyperledger.composer.system.NetworkAdmin", 
+"participantId":"consortiumAdmin"
+}'
+
+composer identity issue -c admin@identiphone-network -f consortiumAdmin.card -u identiphoneconsortium@gmail.com -a "resource:org.hyperledger.composer.system.NetworkAdmin#consortiumAdmin"
+
+composer card import -f consortiumAdmin.card
+composer network ping -c identiphoneconsortium@gmail.com@identiphone-network
+
+#MEMBER 
 composer participant add -c admin@identiphone-network -d '{
   "$class": "org.example.mynetwork.Member",
   "email": "90aaronbyrne@gmail.com",
@@ -11,13 +24,22 @@ composer identity issue -c admin@identiphone-network -f 90aaronbyrne@gmail.com.c
 
 composer card import -f 90aaronbyrne@gmail.com.card
 composer network ping -c 90aaronbyrne@gmail.com@identiphone-network
+
+#REATILER
 composer participant add -c admin@identiphone-network -d '{
-  "$class": "org.example.mynetwork.Member",
-  "email": "emanuel.macroon@gmail.com",
+  "$class": "org.example.mynetwork.Retailerr",
+  "email": "c15709609@mydit.ie",
   "firstName": "emanuel",
   "lastName": "macroon",
   "ownerId": "102"
 }'
+
+composer identity issue -c admin@identiphone-network -f c15709609@mydit.ie.card -u c15709609@mydit.ie -a "resource:org.example.mynetwork.Retailer#102"
+
+composer card import -f c15709609@mydit.ie.card
+composer network ping -c c15709609@mydit.ie@identiphone-network
+
+# Recycler
 composer participant add -c admin@identiphone-network -d '{
   "$class": "org.example.mynetwork.Member",
   "email": "harry.potter@hogwarts.com",
@@ -25,6 +47,13 @@ composer participant add -c admin@identiphone-network -d '{
   "lastName": "potter",
   "ownerId": "103"
 }'
+
+composer identity issue -c admin@identiphone-network -f harry.card -u newemail -a  "resource:org.example.mynetwork.Recycler#103"
+
+composer card import -f harry.card
+composer network ping -c newemail@identiphone-network
+
+#ADD ASSESTS to the network
 
 composer transaction submit -c admin@identiphone-network -d '{
   "$class": "org.hyperledger.composer.system.AddAsset", 
--- setupNetwork.sh
+++ setupNetwork.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 source ../envvars.txt
+echo "REMOVING PREVIOUS VERSION OF NETWORK"
+rm -r ~/.composer
 
 ~/fabric-dev-servers/startFabric.sh
 ~/fabric-dev-servers/createPeerAdminCard.sh
@@ -8,9 +10,9 @@ source ../envvars.txt
 
 composer archive create -t dir -n .
 
-composer network install --card PeerAdmin@hlfv1 --archiveFile identiphone-network@0.0.10.bna
+composer network install --card PeerAdmin@hlfv1 --archiveFile identiphone-network@0.0.12.bna
 
-composer network start --networkName identiphone-network --networkVersion  0.0.10 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
+composer network start --networkName identiphone-network --networkVersion  0.0.12 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
 
 composer card import --file networkadmin.card
 
