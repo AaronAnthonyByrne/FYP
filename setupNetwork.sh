c1b720ae9a494e90a58a46fd624b155ed5d9d619
Aaron Byrne <c15709609@mydit.ie>
2019-04-09 15:51:33 +0000

    ACL will be the death of me

--- setupNetwork.sh
+++ setupNetwork.sh
@@ -10,9 +10,9 @@ rm -r ~/.composer
 
 composer archive create -t dir -n .
 
-composer network install --card PeerAdmin@hlfv1 --archiveFile identiphone-network@0.0.18.bna
+composer network install --card PeerAdmin@hlfv1 --archiveFile identiphone-network@0.0.19.bna
 
-composer network start --networkName identiphone-network --networkVersion  0.0.18 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
+composer network start --networkName identiphone-network --networkVersion  0.0.19 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
 
 composer card import --file networkadmin.card
 
