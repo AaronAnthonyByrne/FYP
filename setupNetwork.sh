b068f8dc8e4ab8f9c7699de0eafe182185047a61
Aaron Byrne <c15709609@mydit.ie>
2019-04-09 14:26:45 +0000

    making cards work

--- setupNetwork.sh
+++ setupNetwork.sh
@@ -10,9 +10,9 @@ rm -r ~/.composer
 
 composer archive create -t dir -n .
 
-composer network install --card PeerAdmin@hlfv1 --archiveFile identiphone-network@0.0.16.bna
+composer network install --card PeerAdmin@hlfv1 --archiveFile identiphone-network@0.0.17.bna
 
-composer network start --networkName identiphone-network --networkVersion  0.0.16 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
+composer network start --networkName identiphone-network --networkVersion  0.0.17 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
 
 composer card import --file networkadmin.card
 
